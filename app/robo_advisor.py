f7cb19216511bb2f8673ce9944478c63906dfe63
megc1 <46460921+megc1@users.noreply.github.com>
2019-02-24 13:59:21 -0500

    added basic url request and json import

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,8 +1,22 @@
 # app/robo_advisor.py
+#REFERENCE: used Prof. Rossetti's screencast
 
-# TODO: import some modules and/or packages here
+import requests
+import json
 
+
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
+
+response = requests.get(request_url)
+# print(type(response)) # <class 'requests.models.Response'>
+# print(response.status_code) #>200
+# print(response.text)
 # TODO: write some Python code here to produce the desired functionality...
+
+parsed_response = json.loads(response.text)
+breakpoint()
+
 
 print("-----------------------")
 print("STOCK SYMBOL: AMZN")
