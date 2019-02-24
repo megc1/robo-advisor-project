8468d0264b6bdb5e15489daecea95f2637ac412a
megc1 <mac492@georgetown.edu>
2019-02-24 13:42:31 -0500

    add requests import

---
+++ app/robo_advisor.py
@@ -0,0 +1,23 @@
+# app/robo_advisor.py
+
+
+import requests
+
+
+request_url = "____________"
+
+response = requests.get(request_url)
+
+
+# TODO: write some Python code here to produce the desired functionality...
+
+print("-----------------------")
+print("STOCK SYMBOL: AMZN")
+
+print("-----------------------")
+print("CRUNCHING THE DATA...")
+
+print("-----------------------")
+print("LATEST CLOSING PRICE: $1,259.19")
+
+# ... etc.
