cd1d2f73e75b7669500ff371cc7a02e9027f9dc9
megc1 <46460921+megc1@users.noreply.github.com>
2019-02-24 13:40:08 -0500

    typo fix

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,13 +4,13 @@
 
 # TODO: write some Python code here to produce the desired functionality...
 
-print("-----------------------)
+print("-----------------------")
 print("STOCK SYMBOL: AMZN")
 
-print("-----------------------)
+print("-----------------------")
 print("CRUNCHING THE DATA...")
 
-print("-----------------------)
+print("-----------------------")
 print("LATEST CLOSING PRICE: $1,259.19")
 
 # ... etc.
