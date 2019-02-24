7a19c6dcbe20663f3b269597d593cd57ac128f62
megc1 <46460921+megc1@users.noreply.github.com>
2019-02-24 13:38:26 -0500

    set up robo_advisor with basic project requirements

---
+++ app/robo_advisor.py
@@ -0,0 +1,16 @@
+# app/robo_advisor.py
+
+# TODO: import some modules and/or packages here
+
+# TODO: write some Python code here to produce the desired functionality...
+
+print("-----------------------)
+print("STOCK SYMBOL: AMZN")
+
+print("-----------------------)
+print("CRUNCHING THE DATA...")
+
+print("-----------------------)
+print("LATEST CLOSING PRICE: $1,259.19")
+
+# ... etc.
