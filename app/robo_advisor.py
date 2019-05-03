99686530345519cf7773940db6d2ac02e484a722
megc1 <mac492@georgetown.edu>
2019-05-03 02:03:46 -0400

    project revisit - first local commit

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,23 +1,158 @@
 # app/robo_advisor.py
-
+#REFERENCE: used Prof. Rossetti's screencast
 
 import requests
+import json
+from datetime import datetime
+import csv
+import os
+from dotenv import load_dotenv
 
 
-request_url = "____________"
+#function adapted from previous projects/Prof.Rossetti's screencast
+#Basic Challenge: formatting prices (done)
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
 
-response = requests.get(request_url)
+api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+
+#USER INPUT VALIDATION:
+while True:
+    ticker_symbol = input("Which stock would you like to evaluate? Please enter its ticker symbol here: ")
+    #Makes sure input values are alphabetical
+    #referenced Geeks for Geeks: https://www.geeksforgeeks.org/python-string-isalpha-application/
+    if not ticker_symbol.isalpha():
+        print("That doesn't seem to be a valid stock symbol. Please check its formatting and try again. ")
+    else:
+        #Referenced os module notes: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/7b43ab256e6b79f231f56c0bbf29025325a9414d/notes/python/modules/os.md
+        request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker_symbol}&apikey={api_key}"
+        response = requests.get(request_url)
+        #Referenced: http://docs.python-requests.org/en/master/user/quickstart/
+        #adapted Error checking from Hiep's solution: https://github.com/hiepnguyen034/robo-stock/blob/master/robo_advisor.py 
+    if "error" in response.text:
+        print("Uh oh! Looks like that stock isn't here. Please restart the program and try another if you'd like.")
+    else:
+        break
+
+while True:
+    acceptable_risk = input("How much risk are you willing to accept? Please enter a number between one and ten, with one being very low risk and ten being very high risk. ")
+    if 1<= float(acceptable_risk) <= 10 :
+       break 
+    else:
+        print("Sorry, that is not a valid level of risk. Please try again.")  
+
+parsed_response = json.loads(response.text)
+tsd = parsed_response["Time Series (Daily)"]
+dates = list(tsd.keys()) #assuming first day is in 0 position, may need to sort if not
+latest_day = dates[0]
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+latest_close = parsed_response["Time Series (Daily)"][latest_day]["4. close"]
+#Referenced notes on dictionaries: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/master/notes/python/datatypes/dictionaries.md
+high_prices = []
+low_prices = []
+for date in dates:
+    high_price = float(tsd[date]["2. high"])
+    high_prices.append(high_price)
+    low_price = float(tsd[date]["3. low"])
+    low_prices.append(low_price)
+
+recent_high = max(high_prices)
+recent_low = min(low_prices)
 
 
-# TODO: write some Python code here to produce the desired functionality...
+    
+#breakpoint()
+
 
 print("-----------------------")
-print("STOCK SYMBOL: AMZN")
-
+print("STOCK SYMBOL: " + ticker_symbol)
 print("-----------------------")
-print("CRUNCHING THE DATA...")
-
+print("REQUESTING STOCK MARKET DATA...")
+#Referenced datetime documentation: https://docs.python.org/3/library/datetime.html
+#Referenced datetime strftime behavior documentation: https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
+print("REQUEST AT: ", datetime.now().strftime('%m-%d-%Y %H:%M:%S'))
 print("-----------------------")
-print("LATEST CLOSING PRICE: $1,259.19")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
+print("-----------------------")
+recommend = " "
+risk_percentage = float(acceptable_risk)/20
+if (float(latest_close) - float(recent_low))/float(recent_low) > risk_percentage:
+    recommend = "Do not buy. Stock risk as calculated based on latest closing price and recent lowest price is higher than desired."
+else:
+    recommend = "Buy! Risk as calculated based on latest closing price and recent lowest price is within preferred range."
 
-# ... etc.
+print("RECOMMENDATION: " + recommend)
+print("-----------------------")
+
+
+#Referenced csv notes: https://github.com/prof-rossetti/georgetown-opim-243-201901/blob/e2d64e2d74621f3ff070175954878ba3f1562388/notes/python/modules/csv.md
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+csv_headers = ["timestamp", "open", "high", "low", "close", "volume" ]
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+    writer.writeheader()
+    for date in dates: 
+        prices = tsd[date]
+        writer.writerow({
+            "timestamp": date,
+            "open": prices["1. open"],
+            "high": prices["2. high"],
+            "low": prices["3. low"],
+            "close": prices["4. close"],
+            "volume": prices["5. volume"]
+    })
+
+print("WRITING DATA TO CSV: " + str(csv_file_path))
+print("-----------------------")
+while True:
+     show_graph = input("Would you like to view a graphical representation of this stock's price activity? Enter Y or N.")
+     if show_graph != "Y" and show_graph != "N":
+         print("Sorry, that's not a valid response. Please enter Y or N.")
+     else:
+         if show_graph == "N":
+                break
+         if show_graph == "Y":
+                print("Note: Once you have viewed your results, please close the graph window to end the program.") #show() function presents script from continuing until window is closed -- haven't found a workaround for that so adapted user behavior for now
+                #REFERENCED: https://matplotlib.org/users/pyplot_tutorial.html
+                import matplotlib
+                import matplotlib.pyplot as plt
+                import matplotlib.ticker as ticker
+
+                plotprices = []
+                #Referenced: https://www.programiz.com/python-programming/methods/built-in/sorted
+                plotdates = sorted(dates)
+
+                for d in dates:
+                    close_price = tsd[d]['4. close']
+                    plotprices.append(float(close_price))
+
+                fig, ax = plt.subplots()
+
+                #Axis formatting adapted from: https://matplotlib.org/gallery/ticks_and_spines/major_minor_demo.html
+                #Partly adapted from https://www.programcreek.com/python/example/100917/matplotlib.ticker.LinearLocator
+                ax.xaxis.set_major_locator(plt.LinearLocator(8))
+                
+ 
+                #Adapted from: https://matplotlib.org/gallery/pyplots/dollar_ticks.html
+                formatter = ticker.FormatStrFormatter('$%1.2f')
+                ax.yaxis.set_major_formatter(formatter)
+                
+                
+                plt.plot(plotdates, plotprices)
+                #REFERENCED: https://matplotlib.org/gallery/subplots_axes_and_figures/figure_title.html
+                plt.xlabel('Date')
+                plt.ylabel('Close Price')
+                plt.title('Closing Stock Prices: ' + ticker_symbol)
+                plt.show()
+                print("Happy investing!")
+                print("-----------------------")
+                break
+                
+# import matplotlib.pyplot as plt
+# plt.plot([1,2,3,4])
+# plt.ylabel('some numbers')
+# plt.show()
+ 
