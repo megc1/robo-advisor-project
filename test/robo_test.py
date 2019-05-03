6a602b5db8bf3b49dfaeadc9947e1d56e929e428
megc1 <mac492@georgetown.edu>
2019-05-03 13:09:51 -0400

    write_to_csv testing

--- test/robo_test.py
+++ test/robo_test.py
@@ -60,3 +60,20 @@ def test_transform_response():
     ]
     assert transform_response(parsed_response) == transformed_response
 
+def test_write_to_csv():
+    test_rows = [
+        {"timestamp": "2019-06-08", "open": "101.0924", "high": "101.9500", "low": "100.5400", "close": "101.6300", "volume": "22165128"},
+        {"timestamp": "2019-06-07", "open": "102.6500", "high": "102.6900", "low": "100.3800", "close": "100.8800", "volume": "28232197"},
+        {"timestamp": "2019-06-06", "open": "102.4800", "high": "102.6000", "low": "101.9000", "close": "102.4900", "volume": "21122917"},
+        {"timestamp": "2019-06-05", "open": "102.0000", "high": "102.3300", "low": "101.5300", "close": "102.1900", "volume": "23514402"},
+        {"timestamp": "2019-06-04", "open": "101.2600", "high": "101.8600", "low": "100.8510", "close": "101.6700", "volume": "27281623"},
+        {"timestamp": "2019-06-01", "open": '99.2798',  "high": "100.8600", "low": "99.1700",  "close": "100.7900", "volume": "28655624"}
+    ]
+    #referenced and adapted row data from Prof. Rossetti's solution
+    csv_file_path = os.path.join(os.path.dirname(__file__), "test_reports", "test_prices.csv")
+    #TODO: add test_reports and test_prices csv files to test on
+    if os.path.isfile(csv_file_path):
+        os.remove(csv_file_path)
+    result = write_to_csv(test_rows, csv_file_path)
+    assert result == True
+    assert os.path.isfile(csv_file_path) == True
