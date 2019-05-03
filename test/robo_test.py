01328472595313006bac9af833adfffc07ccae37
megc1 <mac492@georgetown.edu>
2019-05-03 03:56:35 -0400

    fixed test to check for ticker/base url in compiled url

--- test/robo_test.py
+++ test/robo_test.py
@@ -1,9 +1,14 @@
 import pytest
+import os
 
 from app.robo_advisor import to usd(), compile_url()
+
+
 
 def test_to_usd():
     assert to_usd(12312312) == "$123,123.12"
 
 def test_compile_url():
-    assert compile_url(AMZN) == f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker_input}&apikey={api_key}"
+    assert 'AMZN' in compile_url(AMZN)
+    assert 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' in compile_url(AMZN)
+    
