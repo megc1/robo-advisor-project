3e0f2aaef1ce6178bc08a5447f2253f807e96435
megc1 <mac492@georgetown.edu>
2019-05-03 03:52:47 -0400

    compile URL test

--- test/robo_test.py
+++ test/robo_test.py
@@ -1,6 +1,9 @@
 import pytest
 
-from app.robo_advisor import to usd()
+from app.robo_advisor import to usd(), compile_url()
 
 def test_to_usd():
     assert to_usd(12312312) == "$123,123.12"
+
+def test_compile_url():
+    assert compile_url(AMZN) == f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker_input}&apikey={api_key}"
